f7072ce5f113648cf2108676e2e996a127ae38e8
Yuriy Ivanenko <yuriy.ivanenko@powerhrg.com>
2024-05-15 09:21:49 -0400

    POST data function

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// Add your code here
+const postData = {
+  name: "Yuriy",
+  email: "yuriy.ivanenko@powerhrg.com",
+};
+
+const handlePostRespone = (data) => {
+  const div = document.querySelector('#respone_data')
+  const p = document.createElement('p')
+  p.textContent = `Respone ID: ${data.id}`
+  div.appendChild(p)
+} 
+
+const handlePostError = (error) => {
+  const div = document.querySelector('#respone_data')
+  const p = document.createElement('p')
+  p.textContent = `Error: ${error}`
+  div.appendChild(p)
+}
+
+const submitData = (user_name, user_email) => {
+  const reqBody = {
+    name: user_name,
+    email: user_email
+  }
+  const headers = {
+    'Content-Type': 'application/json',
+    'Accept': 'application/json'
+  }
+return fetch('http://localhost:3000/users',{
+      method: "POST",
+      headers: headers,
+      body: JSON.stringify(reqBody)
+  })
+  .then(res => res.json())
+  .then(handlePostRespone)
+  .catch(handlePostError)
+}
